7b05a947c42795a198b6b6f4876fadea20fe4829
luoyuqinlaura <84111527+luoyuqinlaura@users.noreply.github.com>
2024-02-08 14:23:26 -0800

    REST APIs - Configure for read only

--- 02-backend/springboot-ecommerce/src/main/java/com/yuqin/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/springboot-ecommerce/src/main/java/com/yuqin/ecommerce/config/MyDataRestConfig.java
@@ -1,6 +1,23 @@
 package com.yuqin.ecommerce.config;
 
+import com.yuqin.ecommerce.Entity.Product;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.POST};
+
+//        Spring Data REST builds on top of Spring Data repositories to expose JPA.
+//        It automatically expose Spring Data repositories as RESTful endpoints without the need for explicit controller code
+//        However, we want product can be read only
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+    }
 }
