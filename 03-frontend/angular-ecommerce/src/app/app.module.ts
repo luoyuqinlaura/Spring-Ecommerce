7575d532a69dfc49d331c6704e8ecc00561162ea
luoyuqinlaura <84111527+luoyuqinlaura@users.noreply.github.com>
2024-03-15 13:43:18 -0400

    create angular service to call REST APIs

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './services/product.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, ProductListComponent],
+  imports: [BrowserModule, HttpClientModule],
+  // 这能让我们把这个service inject到other part of application
+  providers: [ProductService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  constructor(private httpClience: HttpClient) {}
+
+  // return an observable, map the JSON data from spring data rest to product array
+  getProductList(): Observable<Product[]> {
+    return this.httpClience
+      .get<GetResponse>(this.baseUrl)
+      .pipe(map((response) => response._embedded.products));
+  }
+}
+
+// supporting interface to help with mapping 14 - 20
+// unwrap the json from spring data rest _embedded entry
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  };
+}
