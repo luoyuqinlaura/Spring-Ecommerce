b79cdbf186ad2507e86ad52e36e77a457099184b
luoyuqinlaura <84111527+luoyuqinlaura@users.noreply.github.com>
2024-03-15 13:55:10 -0400

    develop angular to subscribe to data

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
+  products: Product[] = [];
+  //inject productservice
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    //这个方法是我们自己定义的，调用那边写好的service
+    this.listProducts();
   }
-
+  listProducts() {
+    // method is invoked once you subscribe,这是一个async方法，得到data就可以赋值到我们的products数组中
+    this.productService.getProductList().subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
