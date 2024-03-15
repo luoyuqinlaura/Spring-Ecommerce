9c3a9602316334626f54648101112ca0fc0ab719
luoyuqinlaura <84111527+luoyuqinlaura@users.noreply.github.com>
2024-03-15 12:26:39 -0400

    set up spring data rest

--- 02-backend/springboot-ecommerce/src/main/java/com/yuqin/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/springboot-ecommerce/src/main/java/com/yuqin/ecommerce/config/MyDataRestConfig.java
@@ -1,22 +1,28 @@
 package com.yuqin.ecommerce.config;
 
 import com.yuqin.ecommerce.Entity.Product;
+import com.yuqin.ecommerce.Entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
+@Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.POST};
 
-//        Spring Data REST builds on top of Spring Data repositories to expose JPA.
-//        It automatically expose Spring Data repositories as RESTful endpoints without the need for explicit controller code
-//        However, we want product can be read only
+//        disable HTTP requests for Producy: PUT, POST and DELETE
         config.getExposureConfiguration()
                 .forDomainType(Product.class)
-                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // disable HTTP requests for Producy: PUT, POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
     }
---
+++ SET UP backend.md
@@ -0,0 +1,16 @@
+# SET UP backend
+
+![IMG_0288](/Users/biubiubiu/Downloads/IMG_0288.jpg)
+
+
+
+4. using spring data jpa will automatically create endpoints for our entity.
+   * we have get, put, delete, update, patch(更新一部分) methods.
+
+![IMG_0287](/Users/biubiubiu/Downloads/IMG_0287.jpg)
+
+
+
+however, we want to restrict user, they can only access some behavior like get. the others are forbidden.
+
+![IMG_0289](/Users/biubiubiu/Downloads/IMG_0289.jpg)
